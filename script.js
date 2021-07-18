57672363b0415537e44c2b775384e3d7371c819b
Shabirara <aghnia.shabira@gmail.com>
2021-07-18 14:13:55 +0700

    draft 2

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let body = document.querySelector("#dark");
 let header = document.querySelector("header");
 let headerLeft = document.querySelector(".header-left");
+let a = document.querySelector("a")
 let topWrapper = document.querySelector(".top-wrapper");
 let contentWrapper = document.querySelector(".content-wrapper");
 let heading = document.querySelector(".heading");
 let skills = document.querySelector(".skills");
+let skillIcon=document.querySelector(".skill-icon")
 let messageWrapper = document.querySelector(".message-wrapper");
 let footer = document.querySelector("footer");
 let btnToggle = document.querySelector("#btn-dark");
@@ -16,16 +18,17 @@ let fishDay = document.querySelector("#fishDay");
 btnToggle.addEventListener("click", handleToggleBtn);
 
 function handleToggleBtn (){
-    if(moon.style.display === "block" && sun.style.display === "none"){
-        moon.style.display = "inline-block";
+    if((moon.style.display === "block" && sun.style.display === "none"), (fishNight.style.display === "inline-block" && fishDay.style.display === "none")){
+        moon.style.display = "block";
         sun.style.display = "none";
-        fishNight.style.display = "inline-block";
-        fishDay.style.display = "none";
+        fishDay.style.display = "inline-block";
+        fishNight.style.display = "none";
     } else {
         moon.style.display = "none";
-        sun.style.display = "inline-block";
-        fishNight.style.display = "none";
-        fishDay.style.display = "inline-block";
+        sun.style.display = "block";
+        fishDay.style.display = "none";
+        fishNight.style.display = "inline-block";
+        
     }
 
       body.classList.toggle("light-mode");
@@ -35,6 +38,7 @@ function handleToggleBtn (){
       contentWrapper.classList.toggle("light-mode");
       heading.classList.toggle("light-mode");
       skills.classList.toggle("light-mode");
+      skillIcon.classList.toggle("light-mode");
       messageWrapper.classList.toggle("light-mode");
       footer.classList.toggle("light-mode");
 }
