373b1d9fb76c4d7a6a31aea9d097b5b41288022d
Shabirara <aghnia.shabira@gmail.com>
2021-07-18 08:26:03 +0700

    base

---
+++ script.js
@@ -0,0 +1,8 @@
+let body = document.querySelector("body");
+let header = document.querySelector("header");
+
+
+      function myFunction() {
+        body.classList.toggle("light-mode");
+        header.classList.toggle("light-mode");
+      }
